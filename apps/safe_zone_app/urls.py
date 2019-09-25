f6a81db22aa662a5665e5cec9b40e2074de79fc5
aoalharbi1 <51276512+aoalharbi1@users.noreply.github.com>
2019-09-25 11:41:15 +0300

    .

--- apps/safe_zone_app/urls.py
+++ apps/safe_zone_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^user_in/edit_my_profile/(?P<user_id>\d+)$',views.edit_user),
     url(r'^edit_my_profile/(?P<user_id>\d+)$',views.edit_my_profile),
     url(r'^admin/show_user/(?P<user_id>\d+)/report/(?P<report_id>\d+)$', views.admin_show_report),
-    url(r'^report/delete$', views.delete_report ),
-    url(r'^report/upload$', views.delete_report ),
+    url(r'^report/delete$', views.delete_report),
+    url(r'^report/upload$', views.delete_report),
     url(r'^', views.default_route)
 ]
